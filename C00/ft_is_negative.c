c1b17282e72b8f9c40aa15f9e48aa53755e8bc0d
Adrianogr <adriano.gomes.rodrigues@gmail.com>
2024-08-07 10:11:45 +0100

    C00 and C01

---
+++ C00/ft_is_negative.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void ft_is_negative(int n)
+{
+    if (n < 0) // Check if the value of 'n' is less than 0
+        write(1, "N", 1); // Write the character 'N' to the standard output if 'n' is negative
+    else // If 'n' is not negative
+        write(1, "P", 1); // Write the character 'P' to the standard output
+}
+
+/* main to run ft_is_negative
+int main(void)
+{
+	ft_is_negative(5);
+	ft_is_negative(0);
+	ft_is_negative(-5);
+	return (0);
+}
+*/
